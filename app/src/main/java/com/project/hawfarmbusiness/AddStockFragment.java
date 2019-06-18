7c608178941cdd9cd8f3350956186d404e932b6e
vishlu <vishpatel24@outlook.com>
2019-06-18 10:09:27 +0530

    Home navigation view added

---
+++ app/src/main/java/com/project/hawfarmbusiness/AddStockFragment.java
@@ -0,0 +1,13 @@
+package com.project.hawfarmbusiness;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddStockFragment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_stock_fragment);
+    }
+}
